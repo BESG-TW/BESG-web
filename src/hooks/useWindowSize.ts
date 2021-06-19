091a0e29c3aee6cd020ab2a4fdbacc28d8923da4
kylemocode <kylemo860617@gmail.com>
2021-06-19 15:04:53 +0800

    feat: useWindowSize hook

---
+++ src/hooks/useWindowSize.ts
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+const isSSR = typeof window == 'undefined';
+
+export default function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({
+    width: isSSR ? 1200 : window.innerWidth,
+    height: isSSR ? 800 : window.innerHeight,
+  });
+
+  const changeWindowSize = () => {
+    setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', changeWindowSize);
+
+    return () => {
+      window.removeEventListener('resize', changeWindowSize);
+    };
+  }, []);
+
+  return windowSize;
+}
