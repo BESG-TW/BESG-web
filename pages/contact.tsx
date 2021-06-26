ded9f12a06f740bef80f6d9da5ba2bcd509e49b0
kylemocode <kylemo860617@gmail.com>
2021-06-26 13:45:46 +0800

    feat: install some dependencies & coming soon page

---
+++ pages/contact.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export default function Contact() {
+  return (
+    <Wrapper>
+      <img src="/coming-soon.jpeg" />
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  min-height: 100vh;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  color: ${(props) => props.theme.red};
+  overflow: hidden;
+`;
